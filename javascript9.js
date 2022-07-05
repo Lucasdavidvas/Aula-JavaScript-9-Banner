5e7e913f7a945013a422958cf4c53dbfa235d74a
Lucas David Vasconcelos <106843855+Lucasdavidvas@users.noreply.github.com>
2022-07-05 15:33:01 -0300

    Add files via upload

---
+++ javascript9.js
@@ -0,0 +1,33 @@
+var slides=new Array("1.jpg","2.jpg","3.jpg");
+var tam=slides.length;
+var satual=1;
+var tmpSlider;
+
+function trocaAutomatica(){
+    satual++;
+    if(satual >= tam){
+        satual=0;
+    }
+    document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+}
+
+function iniciaSlider(){
+    document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+    tmpSlider=setInterval(trocaAutomatica,3000);
+}
+
+function parar(){
+    clearInterval(tmpSlider);
+}	
+
+function troca(dir){
+    satual+=dir;
+    if(satual < 0){
+        satual=2;
+    }else if(satual >= tam){
+        satual=0;
+    }
+    document.getElementById("dvSlider").style.backgroundImage="url('"+slides[satual]+"')";
+    clearInterval(tmpSlider);
+    tmpSlider=setInterval(trocaAutomatica,3000);				
+}
